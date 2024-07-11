e2d2cce2a6ed5841189a7c404a2410adc6b9ed5c
shreyareddy <32663313+shreyareddy6@users.noreply.github.com>
2024-07-11 00:02:55 -0400

    displaying current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-// City Input
-const searchInput = document.querySelector("#serach-input");
 const searchButton = document.querySelector("#search-button");
 const apiKey = "370d010cd162e9d8f6adde61521f87cd";
-function searchCity() {
-  const cityName = searchInput.value.trim();
+const cityButtons = document.querySelector(".recent-cities");
+const searchInput = document.querySelector("#search-input");
+
+// get stored cities, if empty then make it an empty array
+const storedCities = JSON.parse(localStorage.getItem("cities")) || [];
+
+function searchCity(newCity) {
+  console.log(newCity);
+  let cityName = newCity;
   console.log(cityName);
   //   5 day forecast
   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
@@ -12,6 +17,10 @@ function searchCity() {
     if (response.ok) {
       response.json().then(function (data) {
         renderCurrentWeather(cityName, data);
+        storeSearchedCities(cityName);
+        displayRecentSearch();
+        console.log(searchInput);
+        // searchInput.value = "";
       });
     } else {
       alert("error");
@@ -21,6 +30,10 @@ function searchCity() {
 
 function renderCurrentWeather(city, weather) {
   console.log(weather);
+  const currentWeatherDiv = document.querySelector("#current-weather");
+
+  currentWeatherDiv.textContent = "";
+
   const temp = weather.list[0].main.temp;
   const wind = weather.list[0].wind.speed;
   const humidity = weather.list[0].main.humidity;
@@ -30,7 +43,6 @@ function renderCurrentWeather(city, weather) {
   console.log(humidity);
   console.log(temp);
 
-  const currentWeatherDiv = document.querySelector("#current-weather");
   const cityField = document.createElement("h2");
   const tempField = document.createElement("p");
   const windField = document.createElement("p");
@@ -38,9 +50,9 @@ function renderCurrentWeather(city, weather) {
   const iconField = document.createElement("img");
 
   cityField.textContent = city;
-  tempField.textContent = temp;
-  windField.textContent = wind;
-  humidityField.textContent = humidity;
+  tempField.textContent = `Temp: ${temp}`;
+  windField.textContent = `Wind:${wind}`;
+  humidityField.textContent = `Humidity: ${humidity}`;
   iconField.setAttribute("src", iconUrl);
 
   currentWeatherDiv.append(
@@ -51,4 +63,45 @@ function renderCurrentWeather(city, weather) {
     humidityField
   );
 }
-searchButton.addEventListener("click", searchCity);
+
+// history of searched cities
+function storeSearchedCities(city) {
+  storedCities.push(city);
+  console.log(storedCities);
+  localStorage.setItem("cities", JSON.stringify(storedCities));
+}
+
+function displayRecentSearch() {
+  const recentSearchDiv = document.querySelector(".recent-cities");
+  recentSearchDiv.textContent = "";
+  console.log("in display");
+  for (city of storedCities) {
+    const cityButton = document.createElement("button");
+    cityButton.setAttribute("class", "recent-cities");
+    cityButton.textContent = city;
+    recentSearchDiv.append(cityButton);
+  }
+}
+
+function handleSearchHistory(e) {
+  //   if (!e.target.matches(".recent-cities")) {
+  //     return;
+  //   }
+  console.log("inside button");
+  console.log(e.target.textContent);
+  //   const input = e.target.textContent;
+  //   console.log("input" + input);
+  searchCity(e.target.textContent);
+}
+
+searchButton.addEventListener("click", () => {
+  let searchInputValue = searchInput.value;
+  if (searchInputValue) {
+    searchCity(searchInputValue);
+    searchInput.value = "";
+    console.log("outside");
+  }
+});
+
+cityButtons.addEventListener("click", handleSearchHistory);
+displayRecentSearch();
